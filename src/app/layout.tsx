b3e1e001196a7ad1c649d94a86296744cb387824
blaze <chenstar125@gmail.com>
2024-04-22 21:32:44 -0400

    develpment branch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
-import "./globals.css";
 import { PropsWithChildren } from "react";
+
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Stratis Payment",
