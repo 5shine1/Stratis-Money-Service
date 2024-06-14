ce43edd00ce7ae9fe0e0c40a4448e9335b551a04
0xsatoshi99 <0xsatoshi99@gmail.com>
2024-06-13 21:10:51 -0400

    update: verify email api integration

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -59,3 +59,13 @@ export const apiResendVerificationEmail = async (email: string) => {
     throw "Something went wrong.";
   }
 };
+
+export const apiConfirmEmail = async (userId: string, code: string) => {
+  try {
+    const result = await axiosInstance.get(`/api/Identity/ConfirmEmail?userId=${userId}&code=${code}`);
+    if (result?.data?.isSucceed === true) return true;
+    return false;
+  } catch (error) {
+    throw error;
+  }
+};
--- src/app/auth/verify-email/success/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const VerifySuccessPage = () => {
-  return <div>VerifySuccessPage</div>;
-};
-
-export default VerifySuccessPage;
