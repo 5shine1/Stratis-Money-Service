87256f8b3209fec03810e0c2249bd662777876b4
blaze <chenstar125@gmail.com>
2024-07-16 08:40:22 -0400

    fix eslint bug

--- src/app/app/account/page.tsx
+++ src/app/app/account/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useId, useState } from "react";
+import React, { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import { Icon } from "@iconify/react";
 
