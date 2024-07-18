407be7a2bfcb2853dc6a87d802b26906e1afb7a0
0xsatoshi99 <0xsatoshi99@gmail.com>
2024-07-18 15:39:59 -0400

    update: kyb request exception

--- src/api/compliance.api.ts
+++ src/api/compliance.api.ts
@@ -2,7 +2,7 @@ import axiosInstance from "@/config/axios";
 
 export const apiStartKYB = async () => {
   try {
-    const result = await axiosInstance.post("/api/Compliance/KnowYourBusiness");
+    const result = await axiosInstance.post("/api/Compliance/KnowYourBusiness", {});
     console.log(result);
     return result?.data;
   } catch (error) {
