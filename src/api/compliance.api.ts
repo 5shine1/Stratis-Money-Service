b4685d6d29ae2406563929e141fc7650a634a09c
blaze <chenstar125@gmail.com>
2024-07-23 12:30:04 -0400

    update: open shufti kyb link on a new tab

--- src/api/compliance.api.ts
+++ src/api/compliance.api.ts
@@ -3,8 +3,7 @@ import axiosInstance from "@/config/axios";
 export const apiStartKYB = async () => {
   try {
     const result = await axiosInstance.post("/api/Compliance/KnowYourBusiness", {});
-    console.log(result);
-    return result?.data;
+    return result.data;
   } catch (error) {
     throw error;
   }
