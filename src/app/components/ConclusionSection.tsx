aa5ec5ad38d691086a42fc7da3c99b1114addf2e
igorgoldobin <IgorGoldobin@ssw.com.au>
2024-08-28 11:48:01 +1000

    feat: landing page update no 4

--- src/app/components/ConclusionSection.tsx
+++ src/app/components/ConclusionSection.tsx
@@ -25,9 +25,10 @@ const ConclusionSection = () => {
           duration={0.5}
         >
           <div className="z-10 ml-0 lg:ml-70 -translate-y-1/2 py-24 lg:py-48 px-36 lg:px-56 bg-gradient-to-b from-primary-200/90 to-primary-500/90 rounded-32 max-w-600 w-full">
-            <h2 className="g-title w-fit">Get Started Now</h2>
+            <h3 className="g-title w-fit">Ready to Accept Crypto Payments?</h3>
             <div className="text-gray-400 mt-16">
-              Lorem ipsum is a pseudo-Latin text used in web design, typography, layout, and printing in place ...
+              Join the growing number of businesses embracing the future of payments.
+              Get started with a secure, compliant solution today.
             </div>
             <Link href={"/auth/register"}>
               <AnimatedSlideButton className=" text-18 lg:text-24 py-12 px-28 lg:px-36 border border-secondary-300 rounded-full mt-36">
