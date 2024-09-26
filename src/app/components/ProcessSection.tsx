642cd05fd3c3c4392c3ff0deace026290772b4f8
0xsatoshi99 <0xsatoshi99@gmail.com>
2024-09-26 10:05:54 -0400

    update: process section ui

--- src/app/components/ProcessSection.tsx
+++ src/app/components/ProcessSection.tsx
@@ -1,20 +1,27 @@
 "use client";
 import React from "react";
 
-import AnimatedSlideButton from "@/components/global/AnimatedSlideButton";
 import ScrollAnimationElement from "@/components/global/ScrollAnimatedElement";
+import IconBox from "@/components/global/IconBox";
 
 const processSteps = [
-  { key: "Register your Business", text: "Sign up and create your business account with us in just a few minutes" },
   {
+    icon: "iconoir:square-cursor",
+    key: "Register your Business",
+    text: "Sign up and create your business account with us in just a few minutes",
+  },
+  {
+    icon: "solar:check-circle-outline",
     key: "Verify your Business",
     text: "Complete the Know your Business verification to ensure compliance with regulations.",
   },
   {
+    icon: "iconoir:link",
     key: "Generate Payment Link",
     text: "Create a payment link through our platform, we will share it with your customer.",
   },
   {
+    icon: "hugeicons:dollar-receive-01",
     key: "Receive Payment & Transfer Funds",
     text: "Once your customer pays, funds are instantly liquidated and ready to be withdrawn to your business bank account",
   },
@@ -23,16 +30,25 @@ const processSteps = [
 const ProcessSection = () => {
   return (
     <section className="px-16 py-80 lg:py-120 relative">
-      <div className="g-effect absolute -top-[200px] -z-10 -left-[800px] w-[1600px] h-[1200px] opacity-80 scale-50 lg:scale-100"></div>
-
       <div className="relative items-center w-full  max-w-600 lg:max-w-1440 mx-auto flex gap-80 flex-col lg:flex-row-reverse">
-        <div className="w-full flex flex-col gap-32">
+        <div className="w-full relative p-12">
+          <ScrollAnimationElement
+            initial={{ opacity: 0, filter: "blur(12px)", x: -40 }}
+            animate={{ opacity: 1, filter: "blur(0px)", x: 0 }}
+            duration={0.8}
+          >
+            <img src="/assets/landing/process-bg.png" alt="" className="w-full" />
+          </ScrollAnimationElement>
+        </div>
+        <div className="w-full flex flex-col gap-12">
           <ScrollAnimationElement
             initial={{ opacity: 0, filter: "blur(12px)", x: 40 }}
             animate={{ opacity: 1, filter: "blur(0px)", x: 0 }}
             duration={0.5}
           >
-            <h2 className="uppercase text-center lg:text-left">How our platform works</h2>
+            <div className="text-52 w-fit g-button-text text-center lg:text-left uppercase font-semibold leading-[1.2]">
+              How our platform works
+            </div>
           </ScrollAnimationElement>
           <ScrollAnimationElement
             initial={{ opacity: 0, filter: "blur(12px)", x: 40 }}
@@ -46,7 +62,7 @@ const ProcessSection = () => {
             </p>
           </ScrollAnimationElement>
 
-          <div className="flex flex-col gap-36">
+          <div className="flex flex-col gap-24 mt-32">
             {processSteps.map((item, i) => (
               <ScrollAnimationElement
                 key={i}
@@ -55,26 +71,18 @@ const ProcessSection = () => {
                 duration={0.5}
                 delay={0.3 + i * 0.2}
               >
-                <div className="flex items-center gap-16">
-                  <div className="rounded-full flex-none w-64 h-64 bg-gradient-to-b from-primary-400 to-secondary-300"></div>
-                  <div className="flex flex-col gap-4">
-                    <p className="g-button-text text-24 font-semibold w-fit">{item.key}</p>
-                    <p className="text-gray-400">{item.text}</p>
+                <div className="flex flex-col lg:gap-24 gap-8 max-w-480 mx-auto lg:mx-0 lg:max-w-none">
+                  <div key={i} className="flex items-start gap-12">
+                    <IconBox icon={item.icon} borderColor="border-[#374F64]" backColor="bg-[#062A42]" />
+                    <div className="flex flex-col gap-6">
+                      <div className="font-semibold text-24">{item.key}</div>
+                      <div className="text-16 text-[#788F99]">{item.text}</div>
+                    </div>
                   </div>
                 </div>
               </ScrollAnimationElement>
             ))}
           </div>
-        </div>
-        <div className="w-full relative p-12">
-          <ScrollAnimationElement
-            initial={{ opacity: 0, filter: "blur(12px)", x: -40 }}
-            animate={{ opacity: 1, filter: "blur(0px)", x: 0 }}
-            duration={0.8}
-          >
-            <img src="/assets/landing/hero-bg.png" alt="" className="absolute w-full" />
-            <img src="/assets/landing/hero.png" alt="" className="relative max-w-full" />
-          </ScrollAnimationElement>
         </div>
       </div>
     </section>
