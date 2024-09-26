9a585a047fde26d779c5e43f9c6acafe4d47726e
0xsatoshi99 <0xsatoshi99@gmail.com>
2024-09-26 10:45:24 -0400

    update: landing footer section

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,40 +2,27 @@
 import React from "react";
 import Link from "next/link";
 import { Icon } from "@iconify/react";
-
-import { APP_ROUTES, SOCIAL_LINKS } from "@/config/constants";
+import SvgLogoApp from "@/assets/SvgLogoApp";
 
 const Footer = () => {
   return (
-    <footer className="w-full relative">
-      <div className="g-effect absolute -top-[500px] -z-10 left-1/2 -translate-x-1/2 w-[1600px] h-[1600px] pointer-event-none scale-50 lg:scale-100"></div>
-
-      <div className="relative px-16 py-24 pb-16 border-b border-white/5">
-        <div className="items-center flex-col lg:flex-row flex gap-24 justify-between max-w-1440 mx-auto w-full">
-          <div className="text-16 font-bold">Stratis Money Service</div>
-          <div className="flex gap-24 items-center">
-            {SOCIAL_LINKS.map((item) => (
-              <Link key={item.id} href={item.link}>
-                <Icon
-                  icon={item.icon}
-                  className="w-20 h-20 text-gray-400 cursor-pointer hover:text-primary-300 u-transition-color"
-                />
-              </Link>
-            ))}
-          </div>
-        </div>
-      </div>
-      <div className="relative flex items-center justify-center gap-48 py-40">
-        {APP_ROUTES.map((item) => (
-          <Link className=" hover:text-secondary-200 u-transition-color" href={item.path} key={item.key}>
-            {item.text}
+    <footer className={`bg-gradient-to-r from-button-from/10 to-button-to/10 z-10 w-full px-16 py-24 backdrop-blur-md`}>
+      <div className="flex items-center gap-8 md:gap-64 max-w-1520 mx-auto">
+        <div className="w-full">
+          <Link href={"/"} className="mr-auto flex gap-10 items-center">
+            <SvgLogoApp className={"w-36 h-36 "} />
+            <span className=" text-24 lg:text-32 font-semibold hidden md:inline g-header">Stratis Money Service</span>
           </Link>
-        ))}
-      </div>
-      <div className="relative border-t border-white/5 px-10 py-24">
-        <div className="w-full text-center mx-auto max-w-1440  text-gray-400 text-12">
-          Copyright © 2024 Stratis Group Ltd. All Right Reserved.
         </div>
+        <div className="flex items-center justify-center gap-40 w-full">
+          <Link className=" hover:text-secondary-200 u-transition-color" href={"#"}>
+            Terms & Conditions
+          </Link>
+          <Link className=" hover:text-secondary-200 u-transition-color" href={"#"}>
+            Privacy Policy
+          </Link>
+        </div>
+        <div className="w-full text-right">©2024 All right reserved</div>
       </div>
     </footer>
   );
