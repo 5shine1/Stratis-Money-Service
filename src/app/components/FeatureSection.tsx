fd1694594ea823a9ddeffcffe86f477e3afdf7d2
0xsatoshi99 <0xsatoshi99@gmail.com>
2024-10-01 16:23:44 -0400

    update: feature section mobile

--- src/app/components/FeatureSection.tsx
+++ src/app/components/FeatureSection.tsx
@@ -11,8 +11,8 @@ const featurePoints = [
 
 const FeatureSection = () => {
   return (
-    <section className="px-16 py-80 lg:py-120 relative">
-      <div className="relative w-full  max-w-600 lg:max-w-1520 mx-auto flex items-center gap-24 justify-between">
+    <section className="px-16 py-40 lg:py-120 relative">
+      <div className="relative w-full  max-w-600 lg:max-w-1520 mx-auto flex items-center flex-col lg:flex-row gap-32 justify-between">
         <div className="w-full max-w-600">
           <ScrollAnimationElement initial={{ opacity: 0, y: -90 }} animate={{ opacity: 1, y: 0 }} duration={0.8}>
             <img src="/assets/landing/feature-bg.png" alt="" className="w-full max-w-full" />
@@ -24,7 +24,7 @@ const FeatureSection = () => {
             animate={{ opacity: 1, scale: 1 }}
             duration={0.5}
           >
-            <div className="text-52 w-fit g-button-text text-center lg:text-left uppercase font-semibold leading-[1.2]">
+            <div className="text-36 md:text-40 lg:text-52 w-fit g-button-text text-center lg:text-left uppercase font-semibold leading-[1.2]">
               Why Choose Us
             </div>
           </ScrollAnimationElement>
@@ -34,7 +34,7 @@ const FeatureSection = () => {
             duration={0.8}
             delay={0.4}
           >
-            <div className="flex flex-col  gap-32 mt-56 lg:mt-60 ">
+            <div className="flex flex-col  gap-32 mt-24 lg:mt-60 ">
               {featurePoints.map((item, i) => (
                 <div
                   key={i}
@@ -42,8 +42,8 @@ const FeatureSection = () => {
                 >
                   <div className="text-[#1B2B35] font-extrabold text-48">0{i + 1}</div>
                   <div className="flex flex-col gap-6">
-                    <div className="font-semibold text-24">{item.title}</div>
-                    <div className="text-16 text-[#788F99]">{item.text}</div>
+                    <div className="font-semibold text-18 md:text-24">{item.title}</div>
+                    <div className="text-14 md:text-16 text-[#788F99]">{item.text}</div>
                   </div>
                 </div>
               ))}
