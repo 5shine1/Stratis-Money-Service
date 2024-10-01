178e6c234220c2a5d9b01e15d285911761a50dba
blaze <chenstar125@gmail.com>
2024-10-01 16:17:34 -0400

    update: hero section mobile

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -14,11 +14,11 @@ const featurePoints = [
 const HeroSection = () => {
   return (
     <section className="relative px-16 pt-120 pb-40 lg:pt-200 lg:pb-60">
-      <img src="/assets/landing/hero-pattern.png" alt="" className="absolute top-0 right-0 h-full" />
+      <img src="/assets/landing/hero-pattern.png" alt="" className="absolute top-0 right-0 h-screen hidden md:block" />
       <div className="relative flex-col lg:flex-row items-center w-full max-w-600 lg:max-w-1520  mx-auto flex gap-60 xl:gap-24">
-        <div className="w-full flex flex-col gap-32 lg:gap-12">
+        <div className="w-full flex flex-col gap-24 lg:gap-12">
           <ScrollAnimationElement initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} duration={0.8}>
-            <div className="text-52 w-fit g-button-text text-center lg:text-left uppercase font-semibold leading-[1.2]">
+            <div className="text-36 md:text-40 lg:text-52 w-fit g-button-text text-left uppercase font-semibold leading-[1.2]">
               Empower Your Business with Secure and Compliant Crypto Payments
             </div>
           </ScrollAnimationElement>
@@ -29,7 +29,7 @@ const HeroSection = () => {
             duration={0.8}
             delay={0.4}
           >
-            <p className="text-center lg:text-left text-16 leading-[1.5]">
+            <p className="text-left font-light md:font-normal md:text-16 leading-[1.5]">
               At Stratis Money Service, we bridge the gap between traditional finance and the digital world. Regulated
               by the Bank of Spain (SEPBLAC) with a VASP registration, we provide a secure platform that allows your
               customers to pay in crypto, ensuring compliance and eliminating uncertainty.
@@ -41,13 +41,13 @@ const HeroSection = () => {
             duration={0.8}
             delay={0.8}
           >
-            <div className="flex flex-col lg:gap-24 gap-8 mt-28 max-w-480 mx-auto lg:mx-0 lg:max-w-none">
+            <div className="flex flex-col lg:gap-24 gap-16 mt-8 md:mt-28">
               {featurePoints.map((item, i) => (
                 <div key={i} className="flex items-start gap-12">
                   <IconBox icon="ph:check-circle" borderColor="border-[#374F64]" backColor="bg-[#062A42]" />
                   <div className="flex flex-col gap-6">
-                    <div className="font-semibold text-24">{item.key}</div>
-                    <div className="text-16 text-[#788F99]">{item.text}</div>
+                    <div className="font-semibold text-20 md:text-24">{item.key}</div>
+                    <div className="text-14 md:text-16 text-[#788F99]">{item.text}</div>
                   </div>
                 </div>
               ))}
