e99bcf074d80c3d58611e345204d7137ae1c6cd8
igorgoldobin <IgorGoldobin@ssw.com.au>
2024-10-14 17:38:24 +1000

    :hammer: updates for new KYB flow

--- src/api/compliance.api.ts
+++ src/api/compliance.api.ts
@@ -2,7 +2,7 @@ import axiosInstance from "@/config/axios";
 
 export const apiStartKYB = async () => {
   try {
-    const result = await axiosInstance.post("/api/Compliance/KnowYourBusiness", {});
+    const result = await axiosInstance.get("/api/Compliance/KnowYourBusiness");
     return result.data;
   } catch (error) {
     throw error;
