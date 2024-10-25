cf8fdb02b038a3e3d4dce426ffd5369bd40f89fd
blaze <chenstar125@gmail.com>
2024-10-25 11:11:04 -0400

    fix: complance role should not access order page

---
+++ src/app/app/(kyb-verified)/order/layout.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { PropsWithChildren } from "react";
+import { ROLES } from "@/@types/common";
+import useAppSelector from "@/hooks/global/useAppSelector";
+import { useRouter } from "next/navigation";
+
+const OrderLayout = ({ children }: PropsWithChildren) => {
+  const { role } = useAppSelector((state) => state.auth);
+  const router = useRouter();
+  if (role === ROLES.COMPLIANCE) {
+    router.push("/404");
+    return;
+  }
+  return children;
+};
+
+export default OrderLayout;
