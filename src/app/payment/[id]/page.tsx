0cef9395d2979f7425a5d7ed2320a7bbfd05a28f
blaze <chenstar125@gmail.com>
2025-01-05 21:18:16 -0500

    fix: connect button switch network

--- src/app/payment/[id]/page.tsx
+++ src/app/payment/[id]/page.tsx
@@ -21,7 +21,7 @@ import { RainbowKitProvider, darkTheme, getDefaultConfig } from "@rainbow-me/rai
 import { metaMaskWallet, rainbowWallet, bitgetWallet, walletConnectWallet } from "@rainbow-me/rainbowkit/wallets";
 import { WagmiProvider } from "wagmi";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-import { mainnet, sepolia } from "viem/chains";
+import { arbitrumSepolia, mainnet, optimismSepolia, polygonAmoy, sepolia } from "viem/chains";
 import ConnectButton from "@/app/components/ConnectButton";
 import "@rainbow-me/rainbowkit/styles.css";
 import { isMobile } from "react-device-detect";
@@ -183,7 +183,7 @@ const PaymentPage: React.FC<Props> = ({ params }) => {
   const config = getDefaultConfig({
     appName: "stratis money service",
     projectId: "YOUR_PROJECT_ID",
-    chains: [sepolia, mainnet],
+    chains: [sepolia, mainnet, polygonAmoy, arbitrumSepolia, optimismSepolia],
     wallets: [
       {
         groupName: "Recommended",
@@ -369,7 +369,7 @@ const PaymentPage: React.FC<Props> = ({ params }) => {
                             <ConnectButton
                               paymentDestination={paymentAddress}
                               amount={paymentAmount}
-                              chain={networkList[network]}
+                              chainProp={networkList[network]}
                               selectedCurrency={selectedCurrency}
                             />
                           )}
