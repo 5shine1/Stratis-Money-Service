54ce86643afc1012c81f088799d832e2fac81c1b
0xsatoshi99 <0xsatoshi99@gmail.com>
2025-02-10 12:36:23 -0500

    feat: no custody wallet component

--- src/app/app/(kyb-verified)/custody/page.tsx
+++ src/app/app/(kyb-verified)/custody/page.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import NoWallet from "./components/NoWallet";
 
 const CustodyPage = () => {
-  return <div>CustodyPage</div>;
+  return <NoWallet />;
 };
 
 export default CustodyPage;
