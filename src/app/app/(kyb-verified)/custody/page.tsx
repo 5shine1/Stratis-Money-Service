c18680434a8fcfbff1dd1294f1f914cb38a9374c
blaze <chenstar125@gmail.com>
2025-02-14 11:22:00 -0500

    custody tab mock ui

--- src/app/app/(kyb-verified)/custody/page.tsx
+++ src/app/app/(kyb-verified)/custody/page.tsx
@@ -1,8 +1,107 @@
-import React from "react";
+"use client";
+import React, { useMemo, useState } from "react";
 import NoWallet from "./components/NoWallet";
+import useAppSelector from "@/hooks/global/useAppSelector";
+import { ROLES } from "@/@types/common";
+import { Icon } from "@iconify/react/dist/iconify.js";
+import Pagination from "rc-pagination";
 
 const CustodyPage = () => {
-  return <NoWallet />;
+  const { locale } = useAppSelector((state) => state.locale);
+  const { role } = useAppSelector((state) => state.auth);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const filteredData = useMemo(() => [], []);
+  return (
+    <div className="flex flex-col gap-24 lg:gap-32 lg:px-48 lg:py-64 py-32 p-8 text-14">
+      <h4 className="w-fit g-header-app">Custody</h4>
+      <div className="flex flex-col gap-32">
+        {role === ROLES.BUSINESS && (
+          <div className="bg-white/5 rounded-8 p-16 py-24 md:p-32 flex items-start gap-8">
+            <div className="flex flex-col gap-6 ">
+              <div className="text-24 text-secondary-200 font-bold mb-16 md:mb-0">Account</div>
+              <div>0x12312312312312312332342</div>
+              <div className="text-24 text-secondary-200 font-bold mt-12 mb-16 md:mb-0">Balance</div>
+              <div className="flex flex-col md:flex-row md:items-center gap-16 md:gap-32 text-14 text-white">
+                <div className="flex  items-center gap-8">
+                  <Icon icon={"cryptocurrency-color:usd"} className="w-24 h-24" />
+                  <span>
+                    123 <span className="opacity-50">USD</span>
+                  </span>
+                </div>
+                <div className="flex items-center gap-8">
+                  <Icon icon={"cryptocurrency-color:eur"} className="w-24 h-24" />
+                  <span>
+                    123 <span className="opacity-50">EUR</span>
+                  </span>
+                </div>
+              </div>
+            </div>
+            <button className="ml-auto w-fit text-button-text font-semibold p-32 text-16 py-16  rounded-12 gap-8 flex items-center justify-center border border-button-border bg-gradient-to-r from-button-from/10 to-button-to/10 transition-all duration-300 hover:from-button-from/50 hover:to-button-to/50">
+              Withdraw
+              <Icon icon={"akar-icons:arrow-cycle"} className="w-16 h-16" />
+            </button>
+          </div>
+        )}
+        {isLoading ? (
+          <div className="text-white/70 p-12 text-center">
+            <Icon icon="eos-icons:three-dots-loading" className="w-64 h-64 mx-auto" />
+          </div>
+        ) : (
+          <>
+            <div className="w-full overflow-x-auto hidden sm:block">
+              <table className="w-full text-white/70">
+                <thead>
+                  <tr className="border-b border-white/10">
+                    <th className="px-8 py-16 text-left w-200">Type</th>
+                    <th className="px-8 py-16 text-left w-160">Amount</th>
+                    <th className="px-8 py-16 text-left w-160">Status</th>
+                    <th className="px-8 py-16 text-left w-160">Timestamp</th>
+                    <th className="px-8 py-16 text-left w-120">Confirmation</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {!filteredData.length ? (
+                    <tr>
+                      <td colSpan={7} className="text-error p-24 text-center">
+                        No History
+                      </td>
+                    </tr>
+                  ) : (
+                    <>
+                      {filteredData.slice(currentPage * 10 - 10, currentPage * 10).map((item, i) => {
+                        return (
+                          <tr key={i} className="odd:bg-[#ffffff04]">
+                            <td className="px-8 py-16">{item.payer}</td>
+                            <td className="px-8 py-16">{item.payer}</td>
+                            <td className="px-8 py-16">{item.payer}</td>
+                            <td className="px-8 py-16">{item.payer}</td>
+                            <td className="px-8 py-16">{item.payers}</td>
+                          </tr>
+                        );
+                      })}
+                    </>
+                  )}
+                </tbody>
+              </table>
+            </div>
+          </>
+        )}
+        <Pagination
+          current={currentPage}
+          onChange={setCurrentPage}
+          showSizeChanger={false}
+          total={filteredData.length}
+          hideOnSinglePage={true}
+          className="flex items-center gap-8 text-14"
+          prevIcon={<Icon icon="icon-park-outline:left" />}
+          nextIcon={<Icon icon="icon-park-outline:right" />}
+          showLessItems
+          showTitle={false}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default CustodyPage;
