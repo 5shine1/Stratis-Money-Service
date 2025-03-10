58137b133e9ba83b40ad8ae77e2286ee1aa000da
blaze <chenstar125@gmail.com>
2025-03-10 16:04:31 -0400

    update: meta for full-width image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,16 +14,24 @@ const RootLayout: React.FC<PropsWithChildren> = ({ children }) => {
   }, []);
   return (
     <html className="notranslate" translate="no">
-      <title>Stratis Money Service</title>
+      <title>Stratis Money Service(Meta test)</title>
+      {/* Open Graph Meta Tags */}
       <meta name="description" content="Stratis Crypto Payment Gateway Platform" />
       <meta property="og:title" content="Stratis Money Service" />
-      <meta property="og:description" content="At Stratis Money Service, we bridge the gap between traditional finance and the digital world..." />
+      <meta property="og:description" content="At Stratis Money Service, we bridge the gap between traditional finance and the digital world. Regulated by the Bank of Spain (SEPBLAC) with a VASP registration, we provide a secure platform that allows your customers to pay in crypto, ensuring compliance and eliminating uncertainty." />
       <meta property="og:url" content={url} />
       <meta property="og:site_name" content="Stratis Money Service" />
       <meta property="og:image" content={`${url}/assets/landing/meta-image.png`} />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
       <meta property="og:type" content="website" />
+      {/* Twitter Card Meta Tags */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="Stratis Crypto Payment Gateway Platform" />
+      <meta name="twitter:description" content="At Stratis Money Service, we bridge the gap between traditional finance and the digital world. Regulated by the Bank of Spain (SEPBLAC) with a VASP registration, we provide a secure platform that allows your customers to pay in crypto, ensuring compliance and eliminating uncertainty." />
+      <meta name="twitter:image" content={`${url}/assets/landing/meta-image.png`}  />
+      <meta name="twitter:image:width" content="1200" />
+      <meta name="twitter:image:height" content="675" />
       <body>{children}</body>
     </html>
   );
